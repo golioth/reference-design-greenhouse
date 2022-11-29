11de3e6d58695cd336eb1e6aab9fcd2577836674
Mike Szczys <mike@golioth.io>
2022-11-29 13:59:56 -0600

    state: add parsing for 'desired' endpoint
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/app_work.c
+++ src/app_work.c
@@ -12,6 +12,8 @@ LOG_MODULE_REGISTER(app_work, LOG_LEVEL_DBG);
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
 #include "app_settings.h"
+#include "json_helper.h"
+#include <zephyr/data/json.h>
 
 static struct golioth_client *client;
 
@@ -122,6 +124,42 @@ static int desired_state_handler(struct golioth_req_rsp *rsp)
 	}
 
 	LOG_HEXDUMP_DBG(rsp->data, rsp->len, "Desired");
+	
+	struct greenhouse_state parsed_state;
+
+	int ret = json_obj_parse((char *)rsp->data, rsp->len,
+			     greenhouse_state_descr,
+				 ARRAY_SIZE(greenhouse_state_descr),
+			     &parsed_state);
+
+	if (ret < 0) {
+		LOG_ERR("Error parsing desired values: %d", -1);
+		return ret;
+	}
+	
+	if (ret & 1<<0) {
+		if ((parsed_state.light >= 0) && (parsed_state.light < 2)) {
+			LOG_DBG("Validated desired Light setting: %d", parsed_state.light);
+		}
+		else if (parsed_state.light == -1) {
+			LOG_DBG("No change requested for Light");
+		}
+		else {
+			LOG_ERR("Invalid desired Light value: %d", parsed_state.light);
+		}
+	}
+	if (ret & 1<<1) {
+		if ((parsed_state.vent >= 0) && (parsed_state.vent < 2)) {
+			LOG_DBG("Validated desired Vent setting: %d", parsed_state.vent);
+		}
+		else if (parsed_state.vent == -1) {
+			LOG_DBG("No change requested for Vent");
+		}
+		else {
+			LOG_ERR("Invalid desired Vent value: %d", parsed_state.vent);
+		}
+	}
+
 	return 0;
 }
 
---
+++ src/json_helper.h
@@ -0,0 +1,16 @@
+#ifndef __JSON_HELPER_H_
+#define __JSON_HELPER_H_
+
+#include <zephyr/data/json.h>
+
+struct greenhouse_state {
+    int8_t light;
+    int8_t vent;
+};
+
+static const struct json_obj_descr greenhouse_state_descr[] = {
+    JSON_OBJ_DESCR_PRIM(struct greenhouse_state, light, JSON_TOK_NUMBER),
+    JSON_OBJ_DESCR_PRIM(struct greenhouse_state, vent, JSON_TOK_NUMBER)
+};
+
+#endif

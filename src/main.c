9cca75d21561f4a5ea2f8a723ea686edc78d57fd
Mike Szczys <mike@golioth.io>
2022-11-30 13:37:13 -0600

    observe: workaround avoids multiple registeration
    Reconnecting to Golioth before the registration process for observed
    data completes will result in multiple observations to the same
    endpoint.
    
    Using the store settings for PSK/PSK-ID prompts a mandatory
    reconnect that often triggers this issue. It's a known problem that will
    be fixed in the SDK. This commit is a workaround until the changes are
    released.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/main.c
+++ src/main.c
@@ -37,10 +37,18 @@ static void golioth_on_connect(struct golioth_client *client)
 {
 	k_sem_give(&connected);
 
-	app_dfu_observe();
-	app_register_settings(client);
-	app_register_rpc(client);
-	app_work_observe();
+	/**
+	 * Workaround for duplicate observations. This will eventually be addressed
+	 * in the Golioth SDK and iteration/conditional can be removed
+	 **/
+	static int iteration = 0;
+	if (iteration > 0) {
+		app_dfu_observe();
+		app_register_settings(client);
+		app_register_rpc(client);
+		app_work_observe();
+	}
+	++iteration;
 }
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
